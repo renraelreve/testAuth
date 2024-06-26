2a5250773da0a928acfa325f122c23e03ba7cf49
Ramadhan <muhdramadhan29@gmail.com>
2024-06-26 18:20:57 +0800

    BookingWithLoggingServiceImplTest.java tested

--- src/test/java/sctp/ntu/booking_api/EventServiceImplTest.java
+++ src/test/java/sctp/ntu/booking_api/EventServiceImplTest.java
@@ -1,5 +1,8 @@
 package sctp.ntu.booking_api;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +18,7 @@ import org.mockito.MockitoAnnotations;
 
 import sctp.ntu.booking_api.entities.Event;
 import sctp.ntu.booking_api.entities.Showtime;
+import sctp.ntu.booking_api.exceptions.EventNotFoundException;
 import sctp.ntu.booking_api.repositories.EventRepository;
 import sctp.ntu.booking_api.repositories.ShowtimeRepository;
 import sctp.ntu.booking_api.serviceImpls.EventServiceImpl;
@@ -37,48 +41,74 @@ public class EventServiceImplTest {
 
     @Test
     public void testFindByDescription_Valid() {
+        //Setup
         Event event = new Event();
+        //Mock
         when(eventRepository.findByDescription(anyString())).thenReturn(event);
+        //Execute
         Event foundEvent = eventServiceImpl.findByDescription("Concert");
+        //Assert
         assertNotNull(foundEvent);
     }
 
     @Test
     public void testFindEventByEid_Valid() {
+        //Setup
         Event event = new Event();
-        when(eventRepository.findEventByEid(anyInt())).thenReturn(event);
+        //Mock
+        when(eventRepository.findById(anyInt())).thenReturn(Optional.of(event));
+        //Execute
         Event foundEvent = eventServiceImpl.findEventByEid(1);
+        //Assert
         assertNotNull(foundEvent);
     }
 
     @Test
     public void testGetAllEvents() {
+        //Setup
         List<Event> events = new ArrayList<>();
         events.add(new Event());
+        //Mock
         when(eventRepository.findAll()).thenReturn(events);
+        //Execute
         List<Event> allEvents = eventServiceImpl.getAllEvents();
+        //Assert
         assertEquals(1, allEvents.size());
     }
 
     @Test
     public void testAddShowtimeToEvent_Valid() {
+        //Setup
         Event event = new Event();
         Showtime showtime = new Showtime();
+        //Mock
         when(showtimeRepository.save(any(Showtime.class))).thenReturn(showtime);
+        //Execute
         Showtime addedShowtime = eventServiceImpl.addShowtimeToEvent(event, showtime, showtimeRepository);
+        //Assert
         assertNotNull(addedShowtime);
     }
 
     @Test
     public void testDeleteAll() {
+        //Execute
         eventServiceImpl.deleteAll();
+        //Verify
         verify(eventRepository, times(1)).deleteAll();
     }
 
     @Test
     public void testDeleteEventByEid_Valid() {
-        doNothing().when(eventRepository).deleteEventByEid(anyInt());
+        //Setup
+        Event event = new Event();
+        Optional<Event> optionalEvent = Optional.of(event);
+        //Mock
+        when(eventRepository.findById(anyInt())).thenReturn(optionalEvent);
+        doNothing().when(eventRepository).deleteById(anyInt());
+        //Execute
         eventServiceImpl.deleteEventbyEid(1);
-        verify(eventRepository, times(1)).deleteEventByEid(1);
+        //Verify
+        verify(eventRepository, times(1)).findById(1);
+        verify(eventRepository, times(1)).deleteById(1);
     }
 }
--- src/test/java/sctp/ntu/booking_api/UserServiceImplTest.java
+++ src/test/java/sctp/ntu/booking_api/UserServiceImplTest.java
@@ -33,66 +33,99 @@ public class UserServiceImplTest {
 
     @Test
     public void testSearchUser_Valid() {
+        //Setup
         List<User> users = new ArrayList<>();
         users.add(new User());
+        //Mock
         when(userRepository.findByName(anyString())).thenReturn(users);
+        //Execute
         List<User> foundUsers = userServiceImpl.searchUser("John");
+        //Assert
         assertEquals(1, foundUsers.size());
     }
 
     @Test
     public void testFindOneUser_Valid() {
+        //Setup
         User user = new User();
+        //Mock
         when(userRepository.findOneByName(anyString())).thenReturn(user);
+        //Execute
         User foundUser = userServiceImpl.findOneUser("John");
+        //Assert
         assertNotNull(foundUser);
     }
 
     @Test
     public void testCreateUser_Valid() {
+        //Setup
         User user = new User();
+        //Mock
         when(userRepository.save(any(User.class))).thenReturn(user);
+        //Execute
         User createdUser = userServiceImpl.createUser(user);
+        //Assert
         assertNotNull(createdUser);
     }
 
     @Test
     public void testGetUser_Valid() {
+        //Setup
         User user = new User();
+        //Mock
         when(userRepository.findById(anyInt())).thenReturn(Optional.of(user));
+        //Execute
         User foundUser = userServiceImpl.getUser(1);
+        //Assert
         assertNotNull(foundUser);
     }
 
     @Test
     public void testGetUser_NotFound() {
+        //Mock
         when(userRepository.findById(anyInt())).thenReturn(Optional.empty());
+        //Execute & Assert
         assertThrows(UserNotFoundException.class, () -> userServiceImpl.getUser(1));
     }
 
     @Test
     public void testGetAllUsers() {
+        //Setup
         List<User> users = new ArrayList<>();
         users.add(new User());
+        //Mock
         when(userRepository.findAll()).thenReturn(users);
+        //Execute
         List<User> allUsers = userServiceImpl.getAllUsers();
+        //Assert
         assertEquals(1, allUsers.size());
     }
 
     @Test
     public void testUpdateUser_Valid() {
+        //Setup
         User user = new User();
         user.setName("UpdatedName");
+        //Mock
         when(userRepository.findById(anyInt())).thenReturn(Optional.of(user));
         when(userRepository.save(any(User.class))).thenReturn(user);
+        //Execute
         User updatedUser = userServiceImpl.updateUser(1, user);
+        //Assert
         assertEquals("UpdatedName", updatedUser.getName());
     }
 
     @Test
     public void testDeleteUser_Valid() {
+        //Setup
+        User user = new User();
+        //Mock
+        when(userRepository.findById(anyInt())).thenReturn(Optional.of(user));
         doNothing().when(userRepository).deleteById(anyInt());
+        //Execute
         userServiceImpl.deleteUser(1);
+        //Verify
+        verify(userRepository, times(1)).findById(1);
         verify(userRepository, times(1)).deleteById(1);
     }
 }
